9fb94ccf6c794843c3219fdff82f37eb3ec30701
sunkh964 <sunkh964@gmail.com>
2024-10-02 14:29:19 +0900

    ddd

---
+++ src/test/java/com/green/Supplier/SupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.Supplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
