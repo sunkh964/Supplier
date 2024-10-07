96cfbb74be1de776660f55fda6e5da6064434e8e
sunkh964 <sunkh964@gmail.com>
2024-10-07 10:46:48 +0900

    241007 1

--- src/main/frontend1/src/pages/admin/salesManage/SalesManage.js
+++ src/main/frontend1/src/pages/admin/salesManage/SalesManage.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const SalesManage = () => {
   return (
-    <div>SalesManage</div>
+    <div className='sales'>
+      <div className='sales-1'>
+        매출 관리
+      </div>
+
+      <div className='sales-2'></div>
+
+      <div className='sales-3'></div>
+    </div>
   )
 }
 
