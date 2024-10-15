0673b9a864bdd849a37256781ea1e943126e390b
nohk1113 <nohk1113@naver.com>
2024-10-15 10:40:02 +0900

    커ë°합체

--- src/main/frontend1/src/App.js
+++ src/main/frontend1/src/App.js
@@ -55,13 +55,10 @@ useEffect(() => {
   )
 }
         </div>
-<<<<<<< HEAD
         <div className='header-content'>
           <h1><i className="bi bi-capsule-pill"></i>그린카페 의약품</h1>
-=======
         <div className='header-content' onClick={(e)=>{navigaite('/')}}>
           <h1><i class="bi bi-capsule-pill"></i>그린카페 의약품</h1>
->>>>>>> nohk
         </div>
       </div>
       <div className='layout-div'>
@@ -88,6 +85,7 @@ useEffect(() => {
           </Route>
         </Routes>
       </div>
+    </div>
     </div>
   );
 }
